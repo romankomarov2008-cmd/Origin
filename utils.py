7f43910a06d0ee4dbc1eeb8fd34bdd3f6ae17f7e
romankomarov2008-cmd <romankomarov2008@gmail.com>
2026-03-03 09:24:52 +0200

    Added python file

---
+++ utils.py
@@ -0,0 +1,18 @@
+def factorial(n):
+	if n ==0 or n == 1:
+		return 1
+	else:
+		return factorial(n - 1) * n
+	print(factorial(5)
+
+import math
+
+def is_prime(n):
+	if n < 2:
+		return False
+	else:
+		for i in range(2, int(math.sqrt(n) + 1):
+			if n % i == 0:
+				return False
+			else:
+				return True
